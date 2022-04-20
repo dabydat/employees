2b9b87a64e9fabdc1467965bf6f907019229774c
David Gómez <alejandrogomez2510@gmail.com>
2022-04-20 10:58:53 -0400

    settings.py default file

--- employees/settings.py
+++ employees/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a#vr_%09b0c^@2a@%qod$d7i38l$z2b$f=bbhbt0vjqtw054t$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
