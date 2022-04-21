5d9b071c04b63d32be57057953693ab097297797
David Gómez <alejandrogomez2510@gmail.com>
2022-04-21 08:42:12 -0400

    adding edit and deleting buttons to the template list

--- applications/employee/views.py
+++ applications/employee/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Employee
 from django.views.generic import (TemplateView,
                                   ListView,
-                                  DetailView
+                                  DetailView,
                                   )
 
 # Create your views here.
